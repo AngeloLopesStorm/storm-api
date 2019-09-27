8edf37e684675efc293e3d4241235aa7e5ec75cd
Angelo Augusto Lopes Santos <angelolopes2158@gmail.com>
2019-09-27 14:06:16 -0300

    Criado classe de conexão

---
+++ src/conexao.php
@@ -0,0 +1,19 @@
+<?php 
+
+    class conexao{
+        var $cr;
+
+        function conectar(){
+
+            $this->cr= new mysqli("localhost","root", "usbw", "storm-db");
+            if( $this->cr->connect_error){
+                die("Erro de conexão com o banco</br>".$this->cr->connect_error);
+            }
+            else{
+                echo"Conectado com sucesso";
+            }
+        }
+
+    }
+
+?>
